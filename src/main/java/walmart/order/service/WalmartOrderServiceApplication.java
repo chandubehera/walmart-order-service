10311beb2c9cae903a2eaeccb023cc6b00679ed4
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-03-11 14:45:36 +0530

    doing first commit to order-service

---
+++ src/main/java/walmart/order/service/WalmartOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package walmart.order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/walmart/order/service/WalmartOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package walmart.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
